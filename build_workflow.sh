ae34e243e54aac08fd2736a24a806120ae7f76be
João Henrique <johnnyonflame@hotmail.com>
2022-02-11 15:15:43 -0300

    Update build_workflow.sh

--- build_workflow.sh
+++ build_workflow.sh
@@ -20,12 +20,13 @@ else
 fi
 
 # Set env config
+export QEMU_CPU=cortex-a35
 export QEMU_LD_PREFIX="/usr/${TRIPLET}"
 export CC="${TRIPLET}-gcc"
 export CXX="${TRIPLET}-g++"
 
 # Configure
-./autogen.sh --prefix="$(pwd)/built" --with-tls=__thread --with-profile4_x=yes --with-runtime-preset=net_4_x,aot --with-csc=mcs --enable-mcs-build --enable-system-aot CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CCFLAGS="${FLAGS}" CC="${TRIPLET}-gcc" CXX="${TRIPLET}-g++"
+./autogen.sh --host=${TRIPLET} --target=${TRIPLET} --prefix="$(pwd)/built" --with-tls=__thread --with-profile4_x=yes --with-runtime-preset=net_4_x,aot --with-csc=mcs --enable-mcs-build --enable-system-aot CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CCFLAGS="${FLAGS}" CC="${TRIPLET}-gcc" CXX="${TRIPLET}-g++"
 
 # Make && Make Install
 make -j$((`nproc`+1))
