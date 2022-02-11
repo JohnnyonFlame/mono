5fb0723dcb8afed5006a33d6f70d0df7baec08aa
João Henrique <johnnyonflame@hotmail.com>
2022-02-11 10:28:22 -0300

    Fix curly braces, quotes, etc

--- build_workflow.sh
+++ build_workflow.sh
@@ -1,24 +1,23 @@
 #!/bin/bash -e
 
-ARG_PLATFORM=$1
-shift
-echo Building for $ARG_PLATFORM...
+ARG_PLATFORM="$1"
+echo Building for ${ARG_PLATFORM}...
 
 VERSION="$(git rev-parse --short HEAD)"
-if [[ x$ARG_PLATFORM == "xlinux/arm/v7" ]]; then
+if [[ x$ARG_PLATFORM == x"linux/arm/v7" ]]; then
   CROSSESSENTIALS=crossbuild-essential-armhf
   TRIPLET=arm-linux-gnueabihf-
   # CMAKE_TOOLCHAIN_FILE=armhf.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
-  SQUASH_NAME=mono-$VERSION-armhf.squashfs
-elif [[ x$ARG_PLATFORM == "xlinux/arm64" ]]; then
+  SQUASH_NAME=mono-${VERSION}-armhf.squashfs
+elif [[ x$ARG_PLATFORM == x"linux/arm64" ]]; then
   CROSSESSENTIALS=crossbuild-essential-aarch64
   TRIPLET=aarch64-linux-gnu-
   # CMAKE_TOOLCHAIN_FILE=aarch64.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
-  SQUASH_NAME=mono-$VERSION-aarch64.squashfs
+  SQUASH_NAME=mono-${VERSION}-aarch64.squashfs
 else
-  echo ERROR:: Unknown platform \"$ARG_PLATFORM\", exiting...
+  echo ERROR: Unknown platform ${ARG_PLATFORM}, exiting...
   exit 1
 fi
 
@@ -26,7 +25,7 @@ fi
 sudo apt-get install git autoconf libtool automake build-essential gettext cmake python3 curl squashfs-tools
 
 # Configure
-./autogen.sh --prefix="$(pwd)/built" --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS="$FLAGS" CXXFLAGS="$FLAGS" CCFLAGS="$FLAGS" CC=$TRIPLETgcc CXX=$TRIPLETg++
+./autogen.sh --prefix="$(pwd)/built" --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CCFLAGS="${FLAGS}" CC="${TRIPLET}gcc" CXX="${TRIPLET}g++"
 
 # Make && Make Install
 make -j$((`nproc`+1))
@@ -35,4 +34,4 @@ make install
 # TODO:: AOT...
 # Package
 cd built
-mksquashfs {bin,etc,include,lib,share} $SQUASH_NAME
+mksquashfs {bin,etc,include,lib,share} "${SQUASH_NAME}"
