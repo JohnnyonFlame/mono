3855b63e0a75a85e73ea992b69db2b253f6947a3
João Henrique <johnnyonflame@hotmail.com>
2022-02-11 11:45:27 -0300

    Update build_workflow.sh

--- build_workflow.sh
+++ build_workflow.sh
@@ -5,12 +5,12 @@ echo Building for ${ARG_PLATFORM}...
 
 VERSION="$(git rev-parse --short HEAD)"
 if [[ x$ARG_PLATFORM == x"linux/arm/v7" ]]; then
-  TRIPLET=arm-linux-gnueabihf-
+  TRIPLET=arm-linux-gnueabihf
   # CMAKE_TOOLCHAIN_FILE=armhf.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
   SQUASH_NAME=mono-${VERSION}-armhf.squashfs
 elif [[ x$ARG_PLATFORM == x"linux/arm64" ]]; then
-  TRIPLET=aarch64-linux-gnu-
+  TRIPLET=aarch64-linux-gnu
   # CMAKE_TOOLCHAIN_FILE=aarch64.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
   SQUASH_NAME=mono-${VERSION}-aarch64.squashfs
@@ -20,7 +20,7 @@ else
 fi
 
 # Configure
-./autogen.sh --prefix="$(pwd)/built" --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CCFLAGS="${FLAGS}" CC="${TRIPLET}gcc" CXX="${TRIPLET}g++"
+./autogen.sh --host="${TRIPLET}" --prefix="$(pwd)/built" --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CCFLAGS="${FLAGS}"
 
 # Make && Make Install
 make -j$((`nproc`+1))
