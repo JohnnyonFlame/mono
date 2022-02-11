db63eebc8be79d87e39fda06f5f9862a17e7cb26
João Henrique <johnnyonflame@hotmail.com>
2022-02-11 10:07:39 -0300

    Specify compilers used

--- build_workflow.sh
+++ build_workflow.sh
@@ -7,22 +7,26 @@ echo Building for $ARG_PLATFORM...
 VERSION="$(git rev-parse --short HEAD)"
 if [[ x$ARG_PLATFORM == "xlinux/arm/v7" ]]; then
   CROSSESSENTIALS=crossbuild-essential-armhf
+  TRIPLET=arm-linux-gnueabihf-
+  # CMAKE_TOOLCHAIN_FILE=armhf.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
-  SQUASH_NAME=mono-$(VERSION)-armhf.squashfs
+  SQUASH_NAME=mono-$VERSION-armhf.squashfs
 elif [[ x$ARG_PLATFORM == "xlinux/arm64" ]]; then
   CROSSESSENTIALS=crossbuild-essential-aarch64
+  TRIPLET=aarch64-linux-gnu-
+  # CMAKE_TOOLCHAIN_FILE=aarch64.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
-  SQUASH_NAME=mono-$(VERSION)-aarch64.squashfs
+  SQUASH_NAME=mono-$VERSION-aarch64.squashfs
 else
   echo ERROR:: Unknown platform \"$ARG_PLATFORM\", exiting...
   exit 1
 fi
 
 # Install deps
-sudo apt-get install git autoconf libtool automake build-essential gettext cmake python3 curl squashfs-tools $CROSSESSENTIALS
+sudo apt-get install git autoconf libtool automake build-essential gettext cmake python3 curl squashfs-tools
 
 # Configure
-./autogen.sh --prefix=$(pwd)/built --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS=$FLAGS CXXFLAGS=$FLAGS CCFLAGS=$FLAGS
+./autogen.sh --prefix=$(pwd)/built --with-csc=mcs --with-mcs-build --enable-system-aot CFLAGS=$FLAGS CXXFLAGS=$FLAGS CCFLAGS=$FLAGS CC=$TRIPLETgcc CXX=$TRIPLETg++
 
 # Make && Make Install
 make -j$((`nproc`+1))
