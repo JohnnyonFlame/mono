b6a0014c08e3f59c32c9dfd981742270d78839a3
João Henrique <johnnyonflame@hotmail.com>
2022-02-11 13:34:15 -0300

    Update build_workflow.sh

--- build_workflow.sh
+++ build_workflow.sh
@@ -4,12 +4,12 @@ ARG_PLATFORM="$1"
 echo Building for ${ARG_PLATFORM}...
 
 VERSION="$(git rev-parse --short HEAD)"
-if [[ x$ARG_PLATFORM == x"linux/arm/v7" ]]; then
+if [[ x$ARG_PLATFORM == x"armhf" ]]; then
   TRIPLET=arm-linux-gnueabihf
   # CMAKE_TOOLCHAIN_FILE=armhf.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35 -mfpu=vfpv4"
   SQUASH_NAME=mono-${VERSION}-armhf.squashfs
-elif [[ x$ARG_PLATFORM == x"linux/arm64" ]]; then
+elif [[ x$ARG_PLATFORM == x"aarch64" ]]; then
   TRIPLET=aarch64-linux-gnu
   # CMAKE_TOOLCHAIN_FILE=aarch64.cmake
   FLAGS="-mcpu=cortex-a35 -mtune=cortex-a35"
